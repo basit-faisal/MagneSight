5f3a0cc1651875858d86327059b544d8195eaaca
basit-faisal <68786058+basit-faisal@users.noreply.github.com>
2023-04-07 01:19:34 +0500

    Update splashscr.py
    gave direct address to icon logo and added icon configuration

--- splashscr.py
+++ splashscr.py
@@ -2,11 +2,12 @@ import PySimpleGUI as sg
 from login import *
 import time
 
+
 sg.theme('DarkBlack')
 
 # Define the layout of the splash screen
 layout = [[sg.Text()],
-          [sg.Text('   '),sg.Image(filename='aa.png', size=(400, 200), key='-IMAGE-')],
+          [sg.Text('   '),sg.Image(filename=r'C:\Users\LENOVO\Desktop\ds-proj\code\aa.ico', size=(400, 200), key='-IMAGE-')],
           [sg.Text('Loading', font=('Helvetica', 20, 'bold italic'), justification='center',text_color='green')],
           [sg.ProgressBar(100, orientation='h', size=(40, 20), key='progressbar',bar_color=('green','white'))]
           
@@ -15,6 +16,7 @@ layout = [[sg.Text()],
 
 # Create the splash screen window
 splash_window = sg.Window('MagneSight', layout, no_titlebar=True, finalize=True, alpha_channel=0.83)
+
 
 # Simulate loading process
 for i in range(101):
