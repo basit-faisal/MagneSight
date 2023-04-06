3a65cbcc31f1ac2b26b934a53cc2db23cae9447f
basit-faisal <68786058+basit-faisal@users.noreply.github.com>
2023-04-07 01:20:53 +0500

    Update viz.py
    icon config added

--- viz.py
+++ viz.py
@@ -8,6 +8,7 @@ def disp_viz(*args):
               [sg.Text()]]
     
     window = sg.Window('MagneSight', layout,size=(700,500),alpha_channel=0.85)
+    window.set_icon(r'C:\Users\LENOVO\Desktop\ds-proj\code\aa.ico')
 
 
     while True:
