8ded233c58653abc67c47b2b8e8639d9fced775e
zainnofal <122494643+zainnofal@users.noreply.github.com>
2023-04-07 16:24:23 +0500

    Update optionsscr.py

--- optionsscr.py
+++ optionsscr.py
@@ -3,9 +3,9 @@ from upload_data import *
 from viz import *
 
 def screen_3(name):
-     sg.theme('DarkBlack')
+    sg.theme('DarkBlack')
 
-     layout = [[sg.Text()],
+    layout = [[sg.Text()],
               [sg.Text('                                                   '),sg.Text(f'Welcome {name}!', font=('Helvetica', 20, 'bold'),text_color='green')],
               [sg.Text()],
               [sg.Text()],
@@ -14,24 +14,24 @@ def screen_3(name):
               [sg.Text()],
               [sg.Text()],
               [sg.Text('                                                               '),sg.Button('Visualizations', font=('Helvetica', 12, 'bold'),button_color=('green','black'),size=(11,2))]
-              
               ]
 
-     # Create the main window
-     window = sg.Window('MagneSight', layout,size=(700,500),alpha_channel=0.85)
-     window.set_icon(r'C:\Users\LENOVO\Desktop\ds-proj\code\aa.ico')
+    # Create the main window
+    window = sg.Window('MagneSight', layout,size=(700,500),alpha_channel=0.85)
 
-
-     while True:
+    while True:
         event, values = window.read()
         if event == sg.WIN_CLOSED:
             break
         if event == 'Add Data':
+
+            file_path = sg.popup_get_file('Select a CSV file', file_types=(("CSV Files", "*.csv"),))
+            if file_path:
+                up_config_data(file_path)
             window.close()
-            up_config_data()
         if event == 'Visualizations':
             window.close()
             disp_viz()
-    
-    # Close the main window and exit the program
-     window.close()
+
+    window.close()
+
