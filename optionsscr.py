cfb6a491f4dc844c65a91a8c963f01826b80f6e6
zainnofal <122494643+zainnofal@users.noreply.github.com>
2023-04-21 17:34:02 +0500

    Update optionsscr.py

--- optionsscr.py
+++ optionsscr.py
@@ -1,6 +1,7 @@
 import PySimpleGUI as sg
 from upload_data import *
 from viz import *
+import os
 
 def screen_3(name):
     sg.theme('DarkBlack')
@@ -17,21 +18,39 @@ def screen_3(name):
               ]
 
     # Create the main window
-    window = sg.Window('MagneSight', layout,size=(700,500),alpha_channel=0.85,icon=r' ')
+    window = sg.Window('MagneSight', layout,size=(700,500),alpha_channel=0.85)
+
+    # Initialize the last selected directory path
+    last_dir_path = ''
 
     while True:
         event, values = window.read()
         if event == sg.WIN_CLOSED:
             break
         if event == 'Add Data':
-
-            file_path = sg.popup_get_file('Select a CSV file', file_types=(("CSV Files", "*.csv"),))
+            # Open file explorer and get file path
+            file_path = sg.popup_get_file('Select a CSV file', initial_folder=last_dir_path, file_types=(("CSV Files", "*.csv"),))
+            
             if file_path:
-                up_config_data(file_path)
-            window.close()
+                # Update last selected directory path
+                last_dir_path = os.path.dirname(file_path)
+
+                # Pass file path to upload_data function
+                #up_config_data(file_path)
+                
+            continue
         if event == 'Visualizations':
-            window.close()
-            disp_viz()
+            # Open file explorer and get file path
+            #file_path = sg.popup_get_file('Select a CSV file', initial_folder=last_dir_path, file_types=(("CSV Files", "*.csv"),))
 
+            if file_path:
+                # Update last selected directory path
+                last_dir_path = os.path.dirname(file_path)
+
+                # Call the create_viz function from the viz module
+                create_viz(file_path)
+                
+            continue
+
+    # Close the main window and exit the program
     window.close()
-
