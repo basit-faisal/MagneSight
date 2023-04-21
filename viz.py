e851057f0d5485317d39260f71f2d52160afa473
zainnofal <122494643+zainnofal@users.noreply.github.com>
2023-04-21 19:32:36 +0500

    Update viz.py

--- viz.py
+++ viz.py
@@ -5,7 +5,7 @@ def create_viz(file_path):
     # Load the CSV file into a Pandas DataFrame
     df = pd.read_csv(file_path).head(10)
     #BAR CHART
-    df.plot(kind='bar', x='City', y='Unit price')
+    df.plot(kind='bar', x='City', y='gross income')
     plt.show()
 
     #LINE CHART
@@ -13,18 +13,33 @@ def create_viz(file_path):
     plt.show
     
     #Scatter Plot
-    df.plot(kind='scatter', x='City', y='Unit price')
+    df.plot(kind='scatter', x='Quantity', y='Unit price')
     plt.show()
     
     #Pie Chart
-    df.plot(kind='pie', y='Unit price', labels=df['City'])
+    sales_by_product = df.groupby('Product line')['Total'].sum()
+
+    # Create a pie chart of the sales by product line
+    plt.pie(sales_by_product, labels=sales_by_product.index, autopct='%1.1f%%')
+    plt.title('Sales by Product Line')
     plt.show()
     
     #Box Plot
-    df.plot(kind='box', y='Unit price')
+  
+    # Select the columns you want to create the box plot for
+    boxplot_cols = ['cogs']
+
+    # Create the box plot using the selected columns
+    df[boxplot_cols].plot(kind='box')
+
+    # Set the title and axis labels
+    plt.title('Box Plot of COGS')
+    plt.ylabel('Price/Quantity/Percentage')
     plt.show()
 
     #histogram
+    
     df.plot(kind='hist', y='Unit price', bins=10)
+    plt.title('')
     plt.show()
     
