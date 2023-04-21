085d086d01118f1ba208cd71aee227d13c2557b3
zainnofal <122494643+zainnofal@users.noreply.github.com>
2023-04-21 17:34:41 +0500

    Update viz.py

--- viz.py
+++ viz.py
@@ -1,19 +1,30 @@
-import PySimpleGUI as sg
+import pandas as pd
+import matplotlib.pyplot as plt
 
-def disp_viz(*args):
-    sg.theme('DarkBlack')
+def create_viz(file_path):
+    # Load the CSV file into a Pandas DataFrame
+    df = pd.read_csv(file_path).head(10)
+    #BAR CHART
+    df.plot(kind='bar', x='City', y='Unit price')
+    plt.show()
 
-    layout = [[sg.Text()],
-              [sg.Text('                                                   '),sg.Text(f'Welcome!', font=('Helvetica', 20, 'bold'),text_color='green')],
-              [sg.Text()]]
+    #LINE CHART
+    df.plot(kind='line',x='City', y='Unit price')
+    plt.show
     
-    window = sg.Window('MagneSight', layout,size=(700,500),alpha_channel=0.85,icon=r' ')
-    #window.set_icon(r'C:\Users\LENOVO\Desktop\ds-proj\code\aa.ico')
-
-
-    while True:
-        event, values = window.read()
-        if event == sg.WIN_CLOSED:
-            break
+    #Scatter Plot
+    df.plot(kind='scatter', x='City', y='Unit price')
+    plt.show()
     
-    window.close()
+    #Pie Chart
+    df.plot(kind='pie', y='Unit price', labels=df['City'])
+    plt.show()
+    
+    #Box Plot
+    df.plot(kind='box', y='Unit price')
+    plt.show()
+
+    #histogram
+    df.plot(kind='hist', y='Unit price', bins=10)
+    plt.show()
+    
