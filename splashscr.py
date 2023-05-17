eef0e13989f2f6cfb5757e0d3302e0edb921e365
zainnofal <122494643+zainnofal@users.noreply.github.com>
2023-05-18 03:53:23 +0500

    Update splashscr.py

--- splashscr.py
+++ splashscr.py
@@ -1,27 +1,30 @@
 import PySimpleGUI as sg
 from login import *
 import time
+from PIL import Image
+
+#Define the layout of the splash screen
+layout = [
+    [sg.Text('   ',background_color='#f1efe7')],
+    [sg.Text('   ',background_color='#f1efe7')],
+    [sg.Text('   ',background_color='#f1efe7')],
+    [sg.Text('   ', background_color='#f3ebe3'), sg.Image(filename='logo.png', size=(600, 250), key='-IMAGE-', background_color='#f1efe7')],
+    [sg.Text( font=('League Spartan Thin', 30, 'bold'), justification='center', text_color='#0f0f0f', background_color='#f1efe7')],
+    [sg.ProgressBar(100, orientation='h', size=(40, 20), key='progressbar', bar_color=('#0f0f0f', 'white'))],
+    [sg.Text('   ',background_color='#f1efe7')],
+    [sg.Text('   ',background_color='#f1efe7')],
+
+]
 
 
-sg.theme('DarkBlack')
-
-# Define the layout of the splash screen
-layout = [[sg.Text()],
-          [sg.Text('   '),sg.Image(filename=r'C:\Users\LENOVO\Desktop\ds-proj\code\aa.ico', size=(400, 200), key='-IMAGE-')],
-          [sg.Text('Loading', font=('Helvetica', 20, 'bold italic'), justification='center',text_color='green')],
-          [sg.ProgressBar(100, orientation='h', size=(40, 20), key='progressbar',bar_color=('green','white'))]
-          
-        ]
-          
 
 # Create the splash screen window
-splash_window = sg.Window('MagneSight', layout, no_titlebar=True, finalize=True, alpha_channel=0.83)
-
+splash_window = sg.Window('MagneSight', layout, no_titlebar=True, finalize=True, alpha_channel=0.83, background_color='#f1efe7', element_justification='c')
 
 # Simulate loading process
 for i in range(101):
     event, values = splash_window.read(timeout=10)
-    if event == sg.WIN_CLOSED:
+    if event == sg.WINDOW_CLOSED:
         break
     splash_window['progressbar'].update(i)
     if i == 100:
@@ -32,3 +35,5 @@ for i in range(101):
 splash_window.close()
 
 login_page()
+
+print()
