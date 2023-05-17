0a23c6ac52067adf047afea5b1d4556395fdd848
zainnofal <122494643+zainnofal@users.noreply.github.com>
2023-05-18 03:53:43 +0500

    Update search_query.py

--- search_query.py
+++ search_query.py
@@ -9,27 +9,30 @@ from sklearn import tree
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import OneHotEncoder
 
 
 #prediction library needed
 
 def FinalScreen(file_path):
     
-    sg.theme('DarkBlack')
 
-    layout = [[sg.Text()],
-              [sg.Text('                                               '),sg.Text('X - SEARCH', font=('Helvetica', 20, 'bold'),text_color='green')],
-              [sg.Text()],
-              [sg.Text()],
-              [sg.Text('   '),sg.Text('Invoice ID',font=('Helvetica', 13, 'bold'),text_color='green'),sg.Input(key='name',size=(40,3),text_color='limegreen')],
-              [sg.Text()],
-              [sg.Text()],
-              [sg.Text('                                                    '),sg.Button('Search', font=('Helvetica', 12, 'bold'),button_color=('green','black'),size=(9,1))],
-              [sg.Text()],
-              [sg.Text('                                                    '),sg.Button('Predict', font=('Helvetica', 12, 'bold'),button_color=('green','black'),size=(9,1))]  
+    layout = [[sg.Text(background_color='#f1efe7')],
+              [sg.Text('                                                ', background_color='#f1efe7'),sg.Text('X - SEARCH', font=('League Spartan Thin Black', 20, 'bold'),text_color='black', background_color='#f1efe7')],
+              [sg.Text(background_color='#f1efe7')],
+              [sg.Text(background_color='#f1efe7')],
+              [sg.Text('          ', background_color='#f1efe7'),sg.Text('Invoice ID',font=('League Spartan Thin', 13, 'bold'),text_color='black', background_color='#f1efe7'),sg.Input(key='name',size=(40,3),text_color='black')],
+              [sg.Text(background_color='#f1efe7')],
+              [sg.Text(background_color='#f1efe7')],
+              [sg.Text('                                                      ',background_color='#f1efe7'),sg.Button('Search', font=('League Spartan Thin', 13, 'bold'),button_color=('#f1efe7','black'),size=(9,1))],
+              [sg.Text(background_color='#f1efe7')],
+              [sg.Text('                                                      ', background_color='#f1efe7'),sg.Button('Predict', font=('League Spartan Thin', 13, 'bold'),button_color=('#f1efe7','black'),size=(9,1))]  
              ]
     
-    window = sg.Window('MagneSight', layout,size=(600,400),alpha_channel=0.83,icon=r'C:\Users\LENOVO\Desktop\ds-proj\code\aa.ico')
+    window = sg.Window('MagneSight', layout,size=(600,400),alpha_channel=0.83,icon=r'C:\Users\LENOVO\Desktop\ds-proj\code\aa.ico', background_color='#f1efe7')
     
     df = pd.read_csv(file_path) 
     while True:
@@ -43,9 +46,9 @@ def FinalScreen(file_path):
             # Output all the other values in the row that matches the search value
             if not result_df.empty:
                 output_str = f"Invoice ID: '{search_value}':\n{result_df.iloc[0]}"
-                sg.popup(output_str, title='Search Result')
+                sg.popup(output_str, title='Search Result', background_color='#f1efe7', button_color='black', text_color='black')
             else:
-                sg.popup(f"No rows found with that Invoice ID: '{search_value}'", title='Search Result')
+                sg.popup(f"No rows found with that Invoice ID: '{search_value}'", title='Search Result',background_color='#f1efe7', button_color='black', text_color='black')
 
             pass
         elif event == 'Predict':
@@ -108,6 +111,44 @@ def FinalScreen(file_path):
                filled = True);
             plt.title("Decision Tree for Gender and Payment")
             plt.show()
+            
+            #LAST ML basically visualizing which products does each gender buy, if there were more products in dataset we can easily find out if there is a product males buy and females don't and vicerversa
+            # One-Hot Encode categorical variables
+            encoder = OneHotEncoder()
+            df_encoded = encoder.fit_transform(df[['Gender', 'Product line']]).toarray()
+
+            # Create a separate dataframe with the encoded features
+            df_encoded = pd.DataFrame(df_encoded, columns=encoder.get_feature_names_out(['Gender', 'Product line']))
+            df_cat = df[['Gender', 'Product line']]
+            df_cat.reset_index(drop=True, inplace=True)
+            df_encoded.reset_index(drop=True, inplace=True)
+            df_cat_encoded = pd.concat([df_cat, df_encoded], axis=1)
+
+            # Concatenate the encoded features with the original dataframe
+            df = pd.concat([df, df_encoded], axis=1)
+
+            # Drop unnecessary columns
+            df = df.drop(['Invoice ID', 'Branch', 'City', 'Customer type', 'Unit price', 'Quantity', 'Tax 5%', 'Total', 'Date', 'Time', 'Payment', 'cogs', 'gross margin percentage', 'gross income', 'Rating', 'Gender', 'Product line'], axis=1)
+
+            # Scale the data
+            scaler = StandardScaler()
+            df_scaled = scaler.fit_transform(df)
+
+            # Fit K-Means clustering model
+            kmeans = KMeans(n_clusters=2, random_state=0)
+            kmeans.fit(df_scaled)
+
+            # Add the cluster labels to the dataframe
+            df_cat_encoded['Cluster'] = kmeans.labels_
+
+            # Plot the clusters
+            plt.figure(figsize=(10,10))
+            plt.scatter(df_cat_encoded['Product line'], df_cat_encoded['Gender'], c=df_cat_encoded['Cluster'], cmap='rainbow')
+            plt.title('Clustering customers based on Product line and Gender',fontsize=15,fontweight="bold")
+            plt.xlabel('Product line')
+            plt.ylabel('Gender')
+            plt.show()
+
     window.close()
     
 
